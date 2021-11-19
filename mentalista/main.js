5903d6d9e7d79d3765a6907110af316c3b3f524d
Emerson916 <emersons.a296@gmail.com>
2021-11-19 16:15:13 -0300

    implementando reset no jogo

--- mentalista/main.js
+++ mentalista/main.js
@@ -1,19 +1,25 @@
 const numeroSecreto = parseInt(Math.random() * 11);
 
+
 function Chutar(){
     const resultado = document.getElementById("resultado");
-    
+
     const chute = parseInt(document.getElementById("valor").value);
 
     if(chute == numeroSecreto){
         resultado.innerHTML = "Você acertou";
-    }else if(chute > 10 || chute < 0){
-        resultado.innerHTML = "Você deve digitar um númerode 0 a 10";
-
+      
+    }else if(chute > 10 || chute < 0 ){
+        resultado.innerHTML = "Você deve digitar um número de 0 a 10";
     }else{
-        resultado.innerHTML = "Errou!!,  o número secreto era " + numeroSecreto;
+        resultado.innerHTML = "Errou!!,  o número secreto era " + numeroSecreto; 
     }
 
+    document.getElementById("btnRecarregar").disabled = false;
+}
+
+function Reset(){
+     window.location.reload();
 }
 
 
