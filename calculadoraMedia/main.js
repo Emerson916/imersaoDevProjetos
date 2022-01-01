09ef1d4a08638ec7c0a830e22c8daaed1adc36a6
Emerson916 <emersons.a296@gmail.com>
2022-01-01 09:46:31 -0300

    Atualizando layout

--- calculadoraMedia/main.js
+++ calculadoraMedia/main.js
@@ -1,1 +1,23 @@
-//Tentar deixar para o usuario usar a aplicação
+const nota1 = parseInt(document.getElementById("nota1").value);
+const nota2 = parseInt(document.getElementById("nota2").value);
+const nota3 = parseInt(document.getElementById("nota3").value);
+const nota4 = parseInt(document.getElementById('nota4').value);
+
+//const somaNotas = nota1 + nota2 + nota3 + nota4;
+
+// const calculoMedia = somaNotas / 4;
+
+function Calcular(){
+    //const resultado = document.getElementById("resultado");
+    console.log(nota1);
+    console.log(nota2);
+    console.log(nota3);
+    console.log(nota4);
+
+    //console.log(somaNotas);
+    // if(calculoMedia <= 5){
+    //     console.log("Parabéns, você passou");
+    // }else{
+    //     console.log("Abroror");
+    // }
+}
